c91e9ef23b41c203aa9daccf13efa19103471d2e
Mr-No-bo-dy <o.m.gutsalyuk@gmail.com>
2022-01-10 13:54:55 +0200

    binding form-1 with php

---
+++ reg.php
@@ -0,0 +1,20 @@
+<?php
+
+$name = $_POST["name"];
+$email = $_POST["email"];
+$town = $_POST["town"];
+$comment = $_POST["comment"];
+
+$text = '
+    <h3>User = "'.$name.'"<br></h3>
+    Email = "'.$email.'"<br>
+    Town = "'.$town.'"<br>
+    <br>
+    <i>Comment = "'.$comment.'"</i>
+';
+
+$subj = "Registration";
+$mail = "MrNobodyXampp@Gmail.com";
+mail ($mail, $subj, $text, "Content-type: text/html; charset=utf-8 \r\n");
+
+echo ("Registration complete");
